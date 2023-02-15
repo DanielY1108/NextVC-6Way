c8e773b25b95cdd0f201d2e99c9e37f8769e7584
DanielY1108 <scarlet040@gmail.com>
2023-02-15 23:37:42 +0900

    Initial Commit

---
+++ NextVC-6way/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextVC-6way
+//
+//  Created by JINSEOK on 2023/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
